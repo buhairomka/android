378149a1f3bc69a010e65463fad60d1c58b29c14
buhairomka <buhairomka@ukr.net>
2021-09-22 18:33:25 +0300

    Initial commit

---
+++ 1/app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    Button buttonApply;
+    Button buttonCancel;
+    EditText editText;
+    int fontSize = 10;
+    String outputText = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        textView.setText("Your input:");
+        radioGroup = findViewById(R.id.radioGroup);
+        buttonApply = findViewById(R.id.button_apply);
+        buttonCancel = findViewById(R.id.button_cancel);
+        editText = findViewById(R.id.editTextTextPersonName);
+
+    }
+
+    public void setButtonApply(View view) {
+        final String editTextText = editText.getText().toString();
+        if (!editTextText.equals("")) {
+            outputText = editTextText;
+            textView.setTextSize(fontSize);
+            textView.setText("Your input:\n"+outputText);
+        } else {
+            Toast.makeText(this, "type something...", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void setButtonCancelValue(View view) {
+        textView.setText("Your input:");
+    }
+
+
+    public void checkButton(View view) {
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+        radioButton = findViewById(selectedId);
+        String buttonText = (String) radioButton.getText();
+
+        switch (buttonText) {
+            case "10sp":
+                fontSize = 10;
+                break;
+
+            case "15sp":
+                fontSize = 15;
+                break;
+
+            case "25sp":
+                fontSize = 25;
+                break;
+        }
+    }
+}
